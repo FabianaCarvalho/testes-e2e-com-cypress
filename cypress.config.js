7a1f84f2b241957bb94d6d5ef0418b742a5a23cf
Fabiana Carvalho <you@example.comfabianasagi@yahoo.com.br>
2023-10-23 19:42:12 -0300

    Configure cypress grep

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,11 @@ module.exports = defineConfig({
     requestTimeout: 9000000,
     env: {
       viewportWidthBreakpoint: 768,
-    }
+    },
+    setupNodeEvents(on, config) {
+      require('@cypress/grep/src/plugin')(config)
+      return config
+    },
   },
   video: true,
   projectId: 'uaf818'
